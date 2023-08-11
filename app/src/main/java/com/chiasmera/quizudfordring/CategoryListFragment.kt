aed6c087a9abb2ebe4e35b1e00d7d4e434df657f
Chiasmera <98319072+Chiasmera@users.noreply.github.com>
2023-08-11 21:19:56 +0200

    Comments and code refactoring

--- app/src/main/java/com/chiasmera/quizudfordring/CategoryListFragment.kt
+++ app/src/main/java/com/chiasmera/quizudfordring/CategoryListFragment.kt
@@ -1,47 +1,48 @@
 package com.chiasmera.quizudfordring
 
-import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore.Audio.Radio
 import android.text.Html
 import android.text.Html.FROM_HTML_MODE_LEGACY
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.RadioButton
+import android.widget.Toast
 import androidx.core.view.children
-import androidx.core.view.forEach
-import androidx.lifecycle.Lifecycle
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
-
 import com.chiasmera.quizudfordring.databinding.FragmentCategoryListBinding
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStreamReader
-
 import java.net.HttpURLConnection
-import java.net.URI
 import java.net.URL
-import java.net.URLDecoder
 
-
+/**
+ * Fragment showing a list of categories and a choice between difficulties
+ */
 class CategoryListFragment : Fragment() {
     val TAG: String = "CategoryListFragment"
+
+    /**
+     * Set of all categories fetched from API. Filled onCreate
+     */
     private val categorySet = mutableSetOf<Category>()
+    //binding properties, to make it easier to find views
     private var _binding: FragmentCategoryListBinding? = null
     private val binding get() = _binding!!
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //Fetch list of categories from API. Blocking in order to make sure categorySet gets filled before initial category button creation.
         runBlocking {
             fetchCategories(categorySet)
         }
@@ -49,7 +50,7 @@ class CategoryListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentCategoryListBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -57,125 +58,110 @@ class CategoryListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val selectedRadio: RadioButton =
-            binding.difficultyRadioGroup.findViewById(binding.difficultyRadioGroup.checkedRadioButtonId)
+        val selectedRadio: RadioButton = binding.difficultyRadioGroup.findViewById(binding.difficultyRadioGroup.checkedRadioButtonId)
+        //Creates the list of category buttons, with name and question count for selected difficulty (medium per default)
         createCategoryList(selectedRadio.text.toString())
+        //Sets a click listener that re-creates the list of category buttons when difficulty is changed, in order to update question count for each category
         for (radioButton in binding.difficultyRadioGroup.children) {
             radioButton.setOnClickListener {
                 val button: RadioButton = radioButton as RadioButton
-                createCategoryList(button.text.toString())
+                createCategoryList(button.text.toString().lowercase())
             }
         }
+
     }
 
+    /**
+     * Creates a sorted list of buttons with the name and question count for each category
+     */
+    private fun createCategoryList(difficulty: String) {
 
-    fun createCategoryList(difficulty: String) {
+        //Removes all existing category buttons in the view
         binding.categoryListView.removeAllViews()
 
+        //For each category, creates a button, adds the proper text and a listener, then adds it to the view
         for (category in categorySet.sortedBy { it.toString() }) {
             val button = Button(activity)
-            val categoryCountForDifficulty = if (difficulty.lowercase() == "easy") {
-                category.easyCount
-            } else if (difficulty.lowercase() == "hard") {
-                category.hardCount
-            } else {
-                category.mediumCount
-            }
-            button.text = "${category} (${categoryCountForDifficulty})"
-            button.textAlignment = View.TEXT_ALIGNMENT_TEXT_START
-            button.setOnClickListener {
 
+                 val buttonText= if (difficulty.lowercase() == "easy") {
+                    "$category (${category.easyCount})"
+                } else if (difficulty.lowercase() == "hard") {
+                    "$category (${category.hardCount})"
+                } else {
+                    "$category (${category.mediumCount})"
+                }
+
+            button.text = buttonText
+            button.textAlignment = View.TEXT_ALIGNMENT_TEXT_START
+
+            //onClick listener that fetches question for the given category and difficulty, then navigates to the first question
+            button.setOnClickListener {
                 var questionArray: Array<Question>
 
                 val fragment = this
                 lifecycleScope.launch {
                     questionArray = fetchQuestions(category, difficulty)
 
-                    val action =
-                        CategoryListFragmentDirections.actionCategoryListFragmentToQuestionFragment(
-                            questionArray = questionArray, index = 0
-                        )
+                    val action = CategoryListFragmentDirections.actionCategoryListFragmentToQuestionFragment(
+                        questionArray = questionArray,
+                        index = 0)
                     NavHostFragment.findNavController(fragment).navigate(action)
                 }
 
             }
 
-
             binding.categoryListView.addView(button)
         }
     }
 
+    /**
+     * GET's questions from the API for the given category and difficulty and returns an array of Questions
+     *
+     * Fetches 10 questions if possible.
+     * If there are less than 10 questions with the given parameters, only fetches as many as are possible
+     * @param category Category object the questions should belong to
+     * @param difficulty the currently chosen difficulty as a String
+     * @return an Array of Questions
+     */
     private suspend fun fetchQuestions(
-        category: Category, difficulty: String = "Medium"
+        category: Category,
+        difficulty: String = "Medium"
     ): Array<Question> {
         val list = arrayListOf<Question>()
         withContext(Dispatchers.IO) {
-            var amount = 10
-            if (difficulty.lowercase() == "easy") {
-                amount = if (category.easyCount < 10) {
-                    category.easyCount
-                } else {
-                    10
-                }
-            } else if (difficulty.lowercase() == "hard") {
-                amount = if (category.hardCount < 10) {
-                    category.hardCount
-                } else {
-                    10
-                }
-            } else {
-                amount = if (category.mediumCount < 10) {
-                    category.mediumCount
-                } else {
-                    10
-                }
-            }
-            val url =
-                URL("https://opentdb.com/api.php?amount=${amount}&category=${category.id}&difficulty=${difficulty.lowercase()}")
+            //Sets the amount of questions to get to either 10 or the amount availible, whichever is higher
+            val amount = getMaxQuestionAmount(category, difficulty.lowercase())
+
+            //HTML GET request with Amount, Category ID and difficulty as parameters
+            val url = URL("https://opentdb.com/api.php?amount=${amount}&category=${category.id}&difficulty=${difficulty.lowercase()}")
             val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
             connection.connect()
 
             if (connection.responseCode == 200) {
+                //Reads response and translates to a JSON array of JSON objects
                 val bufferedReader = BufferedReader(InputStreamReader(connection.inputStream))
                 val result = bufferedReader.readLine().trimIndent()
-
                 val jsonObject = JSONObject(result)
                 val jsonArray = jsonObject.getJSONArray("results")
-                for (i in 0 until jsonArray.length()) {
-                    val current = jsonArray.getJSONObject(i)
-                    val category =
-                        Html.fromHtml(current.getString("category"), FROM_HTML_MODE_LEGACY)
-                            .toString()
-                    val type =
-                        Html.fromHtml(current.getString("type"), FROM_HTML_MODE_LEGACY).toString()
-                    val difficulty =
-                        Html.fromHtml(current.getString("difficulty"), FROM_HTML_MODE_LEGACY)
-                            .toString()
-                    val question =
-                        Html.fromHtml(current.getString("question"), FROM_HTML_MODE_LEGACY)
-                            .toString()
-                    val correct =
-                        Html.fromHtml(current.getString("correct_answer"), FROM_HTML_MODE_LEGACY)
-                            .toString()
-                    val incorrectJsonArray = current.getJSONArray("incorrect_answers")
-                    val incorrect = mutableListOf<String>()
-                    for (i in 0 until incorrectJsonArray.length()) {
-                        incorrect.add(
-                            Html.fromHtml(
-                                incorrectJsonArray[i].toString(), FROM_HTML_MODE_LEGACY
-                            ).toString()
-                        )
-//                        incorrect.add(incorrectJsonArray[i].toString())
-                    }
 
-                    list.add(Question(category, type, difficulty, question, correct, incorrect))
+                //translates each JSON object in array into a Question object
+                for (i in 0 until jsonArray.length()) {
+                    list.add(
+                        parseJSONToQuestion(
+                            jsonArray.getJSONObject(i)
+                        )
+                    )
                 }
 
             } else {
+                //Should handle other status codes from the HTML response (but doesn't right now)
+                //Proper handling would include throwing an error, then catching it and possibly showing the user
+                // a (useful) messageallowing them to either try again or choose another category
                 Log.e(
                     TAG,
                     "Failed to fetch questions. Error code: ${connection.responseCode} (${connection.responseMessage})"
                 )
+                Toast.makeText(context, "Something went wrong!",Toast.LENGTH_SHORT).show()
             }
 
         }
@@ -239,4 +225,44 @@ class CategoryListFragment : Fragment() {
         }
     }
 
+    /**
+     * Parses a JSON representation of a question to a Question object
+     * @param jsonObject a JSON object representing a question
+     * @return a Question object
+     */
+    private fun parseJSONToQuestion (jsonObject : JSONObject) : Question {
+        //I had trouble with encoding. Only thing that ended up working was parsing each value seperately.
+        // There is propablæy a smarter way out there
+        val category = Html.fromHtml(jsonObject.getString("category"), FROM_HTML_MODE_LEGACY).toString()
+        val type = Html.fromHtml(jsonObject.getString("type"), FROM_HTML_MODE_LEGACY).toString()
+        val difficulty = Html.fromHtml(jsonObject.getString("difficulty"), FROM_HTML_MODE_LEGACY).toString()
+        val question = Html.fromHtml(jsonObject.getString("question"), FROM_HTML_MODE_LEGACY).toString()
+        val correct = Html.fromHtml(jsonObject.getString("correct_answer"), FROM_HTML_MODE_LEGACY).toString()
+        //Incorrects answers gets parsed to a List of Strings
+        val incorrectJsonArray = jsonObject.getJSONArray("incorrect_answers")
+        val incorrect = mutableListOf<String>()
+        for (i in 0 until incorrectJsonArray.length()) {
+            incorrect.add(
+                Html.fromHtml(
+                    incorrectJsonArray[i].toString(), FROM_HTML_MODE_LEGACY
+                ).toString()
+            )
+        }
+        return Question(category, type, difficulty, question, correct, incorrect)
+    }
+
+    /**
+     * Returns 10 or the amount of questions for the given category and difficulty, if that amount is < 10
+     */
+    private fun getMaxQuestionAmount (category: Category, difficulty: String) : Int {
+        return if (difficulty.lowercase() == "easy" && category.easyCount < 10) {
+            category.easyCount
+        } else if (difficulty.lowercase() == "hard" && category.mediumCount < 10) {
+            category.mediumCount
+        } else if (difficulty.lowercase() == "hard" && category.hardCount < 10) {
+            category.mediumCount
+        } else {
+            10
+        }
+    }
 }
